71221a984a2b01c0008b0fbfd6d8d34e3c543729
Muhammad Illham Hanafi <illhamhanafi@gmail.com>
2021-06-15 21:19:12 +0700

    add middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { indexPage, messagesPage, addMessage } from '../controllers';
+import { modifyMessage } from '../middleware';
 
 const indexRouter = express.Router();
 
@@ -7,6 +8,6 @@ indexRouter.get('/', indexPage);
 
 // add the get messages endpoint
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
